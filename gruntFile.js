1ae6d66e9e0014c4360ce1a5727564c80fef0c5b
Agon Bina <agon@postalbit.com>
2013-12-28 00:21:09 -0600

    Initial commit

---
+++ gruntFile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'sass',
+                    cssDir: 'css',
+                    environment: 'production'
+                }
+            }
+        },
+
+        watch: {
+            source: {
+                files: ['sass/**/*.scss'],
+                tasks: ['compass'],
+                options: {
+                    livereload: true // needed to run LiveReload
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    // Default task(s)
+    grunt.registerTask('default', ['watch']);
+
+};
