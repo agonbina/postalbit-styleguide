f7a8b53c105cb8e5c576cc3bf5166559a169859f
Agon Bina <agon@postalbit.com>
2014-01-04 19:02:17 -0600

    Started styling the landing page

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require "susy"
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "postalbit-styleguide/images"
 javascripts_dir = "javascripts"
 fonts_dir = "fonts"
 
